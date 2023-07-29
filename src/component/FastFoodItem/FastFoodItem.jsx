d9408ad6958abd65838966d12eb741d06986f16a
TanaKala <mr.mohamadmhz@gmail.com>
2023-07-29 10:32:44 +0330

    my change

--- src/component/FastFoodItem/FastFoodItem.jsx
+++ src/component/FastFoodItem/FastFoodItem.jsx
@@ -2,13 +2,16 @@ import Meta from "antd/es/card/Meta"
 import Card from "antd/es/card/Card"
 import { Badge, Space, Button, Divider } from "antd"
 
-export default function FastFoodItem({ name, price, ingredients, imageUrl }) {
+export default function FastFoodItem({ name, price, ingredients, imageUrl, delay }) {
     return (
         <>
-            <Space direction="vertical" size="middle" className="w-full z-10 ">
+            <Space direction="vertical" size="middle"
+                className="w-full z-10 fade-in-horiz opacity-0"
+                style={{ animationDelay: delay + "s" }}
+            >
                 <Badge.Ribbon className="z-20 font-bold py-1" text={`قیمت : ${price} تومان`}></Badge.Ribbon>
                 <Card
-                    className="h-full w-[330px]"
+                    className="h-full w-[330px] "
                     hoverable
                     cover={<div className={`${!imageUrl ? "animate-pulse" : ""} bg-slate-700 w-full`}>
                         <img className="z-20 w-full" alt="example" src={imageUrl} />
--- src/component/FastFoodList/FastFoodList.jsx
+++ src/component/FastFoodList/FastFoodList.jsx
@@ -3,13 +3,15 @@ import FastFoodItem from "../FastFoodItem/FastFoodItem"
 
 export default function FastFoodList({ fastFoodItems }) {
 
+    let delay = 0.1;
     return (
         <>
-            <Row className="gap-5 flex justify-center md:justify-between xl:justify-evenly items-center w-[90%] flex-wrap"style={{margin:'-40px auto'}}>
-                {fastFoodItems.map((fastFoodItems,key) => {
+            <Row className="gap-5 flex justify-center md:justify-between xl:justify-evenly items-center w-[90%] flex-wrap" style={{ margin: '-40px auto' }}>
+                {fastFoodItems.map((fastFoodItems, key) => {
+                    delay += 0.030;
                     return (
                         <Col key={fastFoodItems.id} className="flex justify-center gap-5 ">
-                            <FastFoodItem {...fastFoodItems}/>
+                            <FastFoodItem {...fastFoodItems} delay={delay} />
                         </Col>
                     )
                 })}
